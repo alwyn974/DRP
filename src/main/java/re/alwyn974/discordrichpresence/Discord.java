3a516e28a8ce3abf1a4d65d9cffc63bfca2d93cf
Alwyn974 <47529956+alwyn974@users.noreply.github.com>
2019-05-10 14:36:37 +0400

    Add files via upload

--- src/main/java/re/alwyn974/discordrichpresence/Discord.java
+++ src/main/java/re/alwyn974/discordrichpresence/Discord.java
@@ -3,22 +3,10 @@ package re.alwyn974.discordrichpresence;
 import club.minnced.discord.rpc.DiscordEventHandlers;
 import club.minnced.discord.rpc.DiscordRPC;
 import club.minnced.discord.rpc.DiscordRichPresence;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraft.client.Minecraft;
 
-@Mod(modid = Discord.MODID, name = Discord.NAME, version = Discord.VERSION)
-public class Discord {
-	public static final String MODID = "DRP";
-	public static final String VERSION = "1.0.0";
-	public static final String NAME = "Discord Rich Presence";
 
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-			this.Start();
-	}
-	
+public class Discord {
 	public final DiscordRPC client = DiscordRPC.INSTANCE;
 	public final DiscordRichPresence richpresence = new DiscordRichPresence();
 	Minecraft mc = Minecraft.getMinecraft();
@@ -26,13 +14,13 @@ public class Discord {
 	public void Start() {
 		DiscordEventHandlers event = new DiscordEventHandlers();
 		event.ready = (user) -> System.out.println("Ready!");
-		this.client.Discord_Initialize("YOUR APP ID", event, true, "0");
+		this.client.Discord_Initialize(Main.DISCORD_ID, event, true, "0");
 		this.richpresence.startTimestamp = System.currentTimeMillis() / 1000;
-		this.richpresence.details = "YOUR DETAILS";
-		this.richpresence.largeImageKey = "YOUR IMAGE";
-		this.richpresence.largeImageText = "YOUR TEXT";
-		this.richpresence.smallImageKey = "YOUR IMAGE";
-		this.richpresence.partySize = 0; 
+		this.richpresence.details = Main.DRP_DETAILS;
+		this.richpresence.largeImageKey = Main.DRP_IMAGE_LARGE;
+		this.richpresence.largeImageText = Main.DRP_IMAGE_LARGE_TEXT;
+		this.richpresence.smallImageKey = Main.DRP_IMAGE_SMALL;
+		this.richpresence.partySize = 0;
 		this.richpresence.partyMax = 0;
 		this.richpresence.smallImageText = mc.getSession().getUsername();
 		this.client.Discord_UpdatePresence(richpresence);
@@ -44,23 +32,19 @@ public class Discord {
 					client.Discord_UpdatePresence(richpresence);
 					client.Discord_RunCallbacks();
 					try {
-						//Status in singleplayer
 						 if (mc.isSingleplayer()) {
-							richpresence.state = "In Singleplayer";
+							richpresence.state = Main.DRP_STATE_SOLO;
 							richpresence.partySize = 0;
 							richpresence.partyMax = 0;
 							client.Discord_UpdatePresence(richpresence);
 						}
-						//Status in multiplayer
 						else if (mc.theWorld !=null && mc.getNetHandler() != null) {
-							richpresence.state = "In Multiplayer";
+							richpresence.state = Main.DRP_STATE_MULTIPLAYER;
 							richpresence.partySize = mc.getNetHandler().playerInfoList.size();
 							richpresence.partyMax = mc.getNetHandler().currentServerMaxPlayers;
 							client.Discord_UpdatePresence(richpresence);
-						} 
-						//Status for other
-						else {
-							richpresence.state = "In The Menu";
+						} else {
+							richpresence.state = Main.DRP_STATE_OTHER;
 							richpresence.partySize = 0;
 							richpresence.partyMax = 0;
 							client.Discord_UpdatePresence(richpresence);
---
+++ src/main/java/re/alwyn974/discordrichpresence/Main.java
@@ -0,0 +1,39 @@
+package re.alwyn974.discordrichpresence;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.config.Configuration;
+
+@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
+public class Main {
+	public static final String MODID = "DRP";
+	public static final String VERSION = "1.0.0";
+	public static final String NAME = "Discord Rich Presence";
+	public static String DISCORD_ID, DRP_DETAILS, DRP_IMAGE_LARGE, DRP_IMAGE_LARGE_TEXT, DRP_IMAGE_SMALL, DRP_STATE_SOLO, DRP_STATE_MULTIPLAYER, DRP_STATE_OTHER;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		Configuration cfg = new Configuration(event.getSuggestedConfigurationFile());
+		try {
+			cfg.load();
+			DISCORD_ID = cfg.getString("DISCORD_ID", "discord","0", "Add your Discord App ID");
+			DRP_DETAILS = cfg.getString("DRP_DETAILS", "discord","Minecraft Server", "Add your Discord Details");
+			DRP_IMAGE_LARGE = cfg.getString("DRP_IMAGE_LARGE", "discord","large_image", "Add your Discord Large Image Key");
+			DRP_IMAGE_LARGE_TEXT = cfg.getString("DRP_IMAGE_LARGE_TEXT", "discord","Private Minecraft Server", "Add your Discord Image Large Text");
+			DRP_IMAGE_SMALL = cfg.getString("DRP_IMAGE_SMALL", "discord","small_image", "Add your Discord Small Image Key");
+			DRP_STATE_SOLO = cfg.getString("DRP_STATE_SOLO", "discord","In SinglePlayer", "Add your Text for the Solo State");
+			DRP_STATE_MULTIPLAYER = cfg.getString("DRP_STATE_MULTIPLAYER", "discord","In Multiplayer", "Add your Text for the MultiPlayer State");
+			DRP_STATE_OTHER = cfg.getString("DRP_STATE_OTHER", "discord", "In The Menu", "Add your Text for the Other State");
+		} catch (Exception ex) {
+			event.getModLog().error("Failed to load configuration");
+		} finally {
+			if (cfg.hasChanged()) {
+				cfg.save();
+			}
+		}
+
+		new Discord().Start();
+	}
+
+}
